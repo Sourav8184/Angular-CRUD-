1b9ef269f905a0466b36fa360a49e3045565f43b
Sourav <ksourav662@gmail.com>
2025-01-30 10:58:19 +0530

    Complete initial project setup and base file structure

---
+++ src/component/add-employee/add-employee.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-employee',
+  imports: [],
+  templateUrl: './add-employee.component.html',
+  styleUrl: './add-employee.component.css',
+})
+export class AddEmployeeComponent {}
